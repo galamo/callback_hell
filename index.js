965f515f19f7e0a687776f656c30af21d324450d
noUser <noUser>
2019-08-19 20:11:32 +0300

    get exchange

--- index.js
+++ index.js
@@ -1,30 +1,56 @@
 const config = {
     codeUrl: "https://restcountries.eu/rest/v2/alpha",
+    exchangeUrl: "https://api.exchangeratesapi.io/latest?base="
 }
-//https://api.exchangeratesapi.io/latest?base=ILS
 
 function getCountryByCode(countryCode, callback) {
-
     $.ajax({
         url: `${config.codeUrl}/${countryCode}`,
         method: "GET",
         success: function (country) {
             callback(country)
+        },
+        error: function (err) {
+            console.log("some error")
         }
     })
-
-
-
-
 }
 
-getCountryByCode("ISR", (country) => {
+function getCurrencyExchange(currency, callback) {
+    $.ajax({
+        url: `${config.exchangeUrl}${currency}`,
+        method: "GET",
+        success: function (cRes) {
+            callback(cRes)
+        },
+        error: function (err) {
+            console.log("getCurrencyExchange error")
+        }
+    })
+}
+
+getCountryByCode("USA", (country) => {
 
     country.borders.forEach(item => {
         $("#borders").append(`<div id='${item}' class='loader'></div>`)
-        getCountryByCode(item, (result) => {
-            $(`#${item}`).html(result.name).attr({ class: "" })
-        })
+        setTimeout(() => {
+            getCountryByCode(item, (result) => {
+                if (result.name === "Egypt") {
+                    setTimeout(() => {
+                        $(`#${item}`).html(result.name).attr({ class: "" })
+                    }, 2000);
+                } else {
+                    $(`#${item}`).html(result.name).attr({ class: "" })
+                }
+
+                result.currencies.forEach(coin => {
+                    getCurrencyExchange(coin.code, (exChangeResult) => {
+                        console.log(result.name, exChangeResult.base, exChangeResult.rates.ILS)
+                    })
+                })
+
+            })
+        }, 2000);
 
     })
 

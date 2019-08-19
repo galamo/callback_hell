8a80667b69f3ccac352941041dacfde8c4f09eea
noUser <noUser>
2019-08-19 18:43:11 +0300

    Callback hell

---
+++ index.js
@@ -0,0 +1,27 @@
+const config = {
+    codeUrl: "https://restcountries.eu/rest/v2/alpha",
+}
+//https://api.exchangeratesapi.io/latest?base=ILS
+
+function getCountryByCode(countryCode, callback) {
+
+    $.ajax({
+        url: `${config.codeUrl}/${countryCode}`,
+        method: "GET",
+        success: function (country) {
+            callback(country)
+        }
+    })
+
+}
+
+getCountryByCode("ISR", (country) => {
+    console.log(country.borders[0])
+    getCountryByCode(country.borders[0], (countryBorderA) => {
+        console.log(countryBorderA)
+        getCountryByCode(countryBorderA.borders[0], (countryBorderB) => {
+            console.log(countryBorderB)
+            $("#country").val(countryBorderB.currencies[0].code)
+        })
+    })
+});
